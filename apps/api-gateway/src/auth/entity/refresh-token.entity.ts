26d05df8cd06ffc803be86c9561e90394c37b8c4
hunit <hunit612@naver.com>
2024-03-05 23:49:11 +0900

    auth

---
+++ apps/api-gateway/src/auth/entity/refresh-token.entity.ts
@@ -0,0 +1,18 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class RefreshToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  token: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Column()
+  userId: string;
+}
--- apps/api-gateway/src/user/user.service.ts
+++ apps/api-gateway/src/user/user.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@Inject('USER_SERVICE') private client: ClientProxy) {}
+  async findOneByEmail(email: string) {
+    const pattern = { cmd: 'findOneByEmail' };
+    const payload = email;
+    const { id: userId } = await firstValueFrom<{ id: string }>(
+      this.client.send<{ id: string }>(pattern, payload),
+    );
+    return userId;
+  }
+
+  async create(email: string, password: string) {
+    const pattern = { cmd: 'create' };
+    const payload = { email, password };
+    const { id: userId } = await firstValueFrom<{ id: string }>(
+      this.client.send<{ id: string }>(pattern, payload),
+    );
+    return userId;
+  }
+
+  async validateUser(email: string, password) {
+    return '';
+  }
+}
