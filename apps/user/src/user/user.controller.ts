e5800ed4b9be2e5376d2343aed0cd43f49b8c992
hunit <hunit612@naver.com>
2024-03-16 23:54:08 +0900

    user

--- apps/user/src/user/user.controller.ts
+++ apps/user/src/user/user.controller.ts
@@ -2,11 +2,11 @@ import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
 import { MessagePattern } from '@nestjs/microservices';
 
-@Controller('user')
+@Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @MessagePattern({ cmd: 'findOneByEmail ' })
+  @MessagePattern({ cmd: 'findOneByEmail' })
   async findOneByEmail(email: string): Promise<{ id: string }> {
     const user = await this.userService.findOneByEmail(email);
     return { id: user?.id || null };
--- apps/user/src/user/user.service.ts
+++ apps/user/src/user/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
-import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
